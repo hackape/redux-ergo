cecf1dd3a32ee9b2b3e558e9903c67c3f3b144fd
hackape <muse.vincent@gmail.com>
2018-09-10 19:34:59 +0800

    fix prototype related bugs

--- src/core/worker.ts
+++ src/core/worker.ts
@@ -1,5 +1,5 @@
 import { asMutable, getValue } from 'as-mutable';
-import { isPromise, isGenerator } from '../utils/is';
+import { isPrimitive, isPromise, isGenerator } from '../utils/is';
 import { dispatch } from './middleware';
 import { shouldUpdateState, iterateGenerator } from './effect';
 import { IAction } from './transpile';
@@ -9,7 +9,7 @@ export type IWorkers<S> = {
 };
 
 const getMutableStateWithProto = (plainStateObject, proto) => {
-  const oldProto = plainStateObject.__proto__;
+  const oldProto = plainStateObject && plainStateObject.__proto__;
   plainStateObject.__proto__ = proto;
   const mutableState = asMutable(plainStateObject);
   plainStateObject.__proto__ = oldProto;
@@ -33,15 +33,28 @@ export const workerFactory = (mode, workers, methodName) => {
       if (action.meta && action.meta.finalize) return action.payload;
       const getFinalizeAction = finalizeActionFactory(action);
 
+      let oldProto, mutableState;
+      if (isPrimitive(prevState)) {
+        mutableState = prevState;
+      } else {
+        oldProto = prevState.__proto__;
+        mutableState = getMutableStateWithProto(prevState, workers);
+      }
+
+      const getValueAndRevertProto = (target, proto) => {
+        target = getValue(target);
+        if (target) target.__proto__ = proto;
+        return target;
+      };
+
       // actually call the fn()
-      const mutableState = getMutableStateWithProto(prevState, workers);
       const ret = workers[methodName].apply(mutableState, action.payload);
 
       if (isPromise(ret)) {
         // 1. ret is a promise, meaning worker is async func
         // async threads:
         ret.then(() => {
-          const nextState = getValue(mutableState);
+          const nextState = getValueAndRevertProto(mutableState, oldProto);
           if (nextState !== prevState) dispatch(getFinalizeAction(nextState));
         });
 
@@ -50,7 +63,7 @@ export const workerFactory = (mode, workers, methodName) => {
         // 2. ret is a generator, meaning worker is generator func
         // async threads:
         iterateGenerator(ret, (__, stepId) => {
-          const nextState = getValue(mutableState);
+          const nextState = getValueAndRevertProto(mutableState, oldProto);
           if (nextState !== prevState) {
             const msg = getFinalizeAction(nextState);
             msg.meta.stepId = stepId;
@@ -61,7 +74,7 @@ export const workerFactory = (mode, workers, methodName) => {
         return prevState; // no op in sync thread
       } else {
         // 3. sync worker, normal reducer
-        const nextState = getValue(mutableState);
+        const nextState = getValueAndRevertProto(mutableState, oldProto);
         return nextState;
       }
     };
