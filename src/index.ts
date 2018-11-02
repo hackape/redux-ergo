8d86f7f2a26c5f123a81717b30318cfdb7fb505c
hackape <muse.vincent@gmail.com>
2018-11-02 17:56:27 +0800

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { transpile } from './core/transpile';
-export { getState, middleware as ergoMiddleware, IErgoMiddleware } from './core/middleware';
-export { effectDecorator as effect, setState } from './core/effect';
-export { reduceReducers } from './utils/reduce-reducers';
+export { createService } from './core/createService';
+export { createServiceGateway } from './core/serviceGateway';
+export { getState } from './core/getState';
